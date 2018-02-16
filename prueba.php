8a93d69d48f4a1063d811be5028209bb8ac2d412
Alejandro Castellanos <brian_0827@hotmail.com>
2018-02-16 12:27:55 -0500

    plantilla inicial de la pagina

---
+++ prueba.php
@@ -0,0 +1,21 @@
+<?php
+
+$ho = 'hola';
+$sum = 5;
+
+echo $ho;
+echo "hola";
+
+?>
+
+git init: inicar repositorio
+git clone 'url repo en git': clonar el repositorio localmente
+git status: ver status de los archivos nuevos para subir al repositorio
+git add . : agregar todos los archivos de la carpeta
+git commit -m "detalle de que contiene el repo"
+git remote add origin 'url_de_git_del_repo'
+git push -u origin master : para subir codigo a la carpeta master
+git rm nombre.archivo: eliminar un archivo del repo
+git mv nombre_archivo ubicacion_final: mover un archivo del repo
+git log: confirmaciones hechas en el repo
+git log --stat: estadisticas de confirmaciones hechas en el repo
